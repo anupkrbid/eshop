720d0943f5d2a46f792d72468871c44ef7141782
Anup Kumar Bid <anupbid94@gmail.com>
2022-02-22 01:32:43 +0530

    feat: Auth Implementation and code cleanup

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,21 @@
+function showSignInModal(event) {
+  hideAllModal(event);
+  event.stopPropagation();
+  setTimeout(() => {
+    enigma.openModal("#auth-modal[data-sign-in]");
+  }, 300);
+}
 
+function showSignUpModal(event) {
+  hideAllModal(event);
+  event.stopPropagation();
+  setTimeout(() => {
+    enigma.openModal("#auth-modal[data-sign-up]");
+  }, 300);
+}
 
+function hideAllModal(event) {
+  event.stopPropagation();
+  enigma.closeModal("#auth-modal[data-sign-up]");
+  enigma.closeModal("#auth-modal[data-sign-in]");
+}
